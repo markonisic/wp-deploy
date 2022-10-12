bacc79845bf602daad3d6e1a80c8e2e6de9aa7fe
Marko.Nisic <marko.nisic@htecgroup.com>
2022-10-12 11:08:29 +0200

    Corrected DB query and added here_doc function for query

--- wp-deploy.sh
+++ wp-deploy.sh
@@ -1,50 +1,53 @@
-    #!/bin/bash 
-    # A script which creates a swap file, sets permissions and installs Wordpress with LEMP stack(Nginx, MariaDB, PHP)
+#!/bin/bash 
+# A script which creates a swap file, sets permissions and installs Wordpress with LEMP stack(Nginx, MariaDB, PHP)
 
-		#### Dynamic variables ####
-		#### These should be changed, they are parsed in the commands bellow #### 
-		DB_NAME="test_wp"
-		DB_USER="test_user"
-		DB_PASS="Test.Pass1"
+#### Dynamic variables ####
+#### These should be changed, they are parsed in the commands bellow #### 
+DB_NAME="test_wp"
+DB_USER="test_user"
+DB_PASS="Test.Pass1"
 
-		#### Static variables ####
-		DB_CREATE="$(CREATE DATABASE $DB_NAME; GRANT ALL ON $DB_NAME. * TO '$DB_USER'@'localhost' IDENTIFIED BY '$DB_PASS')"
-		
-		#### System update ####
-    sudo apt update && sudo apt upgrade -y
+#### Static variables ####
 
-		#### Swap file setup ####
-    sudo fallocate -l 4G /swapfile
-    sudo chmod 600 /swapfile
-    sudo mkswap /swapfile
-    sudo swapon /swapfile
+#### System update ####
+sudo apt update && sudo apt upgrade -y
 
-		#### LEMP stack install ####
-    sudo apt install nginx mariadb-server -y
-    sudo apt install php php-common php-gd php-intl php-mysql php-curl php-mbstring php-soap php-xml php-xmlrpc php-fpm php-zip -y
+#### Swap file setup ####
+sudo fallocate -l 4G /swapfile
+sudo chmod 600 /swapfile
+sudo mkswap /swapfile
+sudo swapon /swapfile
 
-		#### Enabling Nginx and MariaDB ####
-    sudo systemctl enable nginx.service
-    sudo systemctl enable mariadb.service
+#### LEMP stack install ####
+sudo apt install nginx mariadb-server -y
+sudo apt install php php-common php-gd php-intl php-mysql php-curl php-mbstring php-soap php-xml php-xmlrpc php-fpm php-zip -y
 
-		#### Wordpress download ####
-    mkdir /var/www/html/wordpress
-    cd /var/www/html/wordpress
-    sudo wget https://wordpress.org/latest.tar.gz
-    sudo tar -xvzf latest.tar.gz
+#### Enabling Nginx and MariaDB ####
+sudo systemctl enable nginx.service
+sudo systemctl enable mariadb.service
 
-		#### Removing the latest.tar file and xmlrpc ####
-    cd wordpress/
-    sudo mv * ..
-    cd ..
-    sudo rm xmlrpc.php
-    sudo rm -r wordpress/
-    sudo rm latest.tar.gz
+#### Wordpress download ####
+mkdir /var/www/html/wordpress
+cd /var/www/html/wordpress
+sudo wget https://wordpress.org/latest.tar.gz
+sudo tar -xvzf latest.tar.gz
 
-		#### Configuring file permissions ####
-    sudo find /var/www/html/wordpress -type d -exec chmod 755 {} \;
-    sudo find /var/www/html/wordpress -type f -exec chmod 644 {} \;
-    sudo chown -R www-data:www-data /var/www/html/wordpress
-		
-		#### Wordpress DB setup ####
-		mysql -u root -p -e <<< "$DB_CREATE"
+#### Removing the latest.tar file and xmlrpc ####
+cd wordpress/
+sudo mv * ..
+cd ..
+sudo rm xmlrpc.php
+sudo rm -r wordpress/
+sudo rm latest.tar.gz
+
+#### Configuring file permissions ####
+sudo find /var/www/html/wordpress -type d -exec chmod 755 {} \;
+sudo find /var/www/html/wordpress -type f -exec chmod 644 {} \;
+sudo chown -R www-data:www-data /var/www/html/wordpress
+
+#### Wordpress DB setup ####
+sudo -i mysql <<QUERY
+CREATE DATABASE $DB_NAME;
+GRANT ALL ON $DB_NAME. * TO '$DB_USER'@'localhost' IDENTIFIED BY '$DB_PASS';
+QUERY
+
